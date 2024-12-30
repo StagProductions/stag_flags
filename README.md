019393071cc219c0cddd559231f95d502dd84500
Stag Productions <rhysdear33@gmail.com>
2024-12-30 20:37:27 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# stag_flags
+A simple replace to turn American Flags into British.
+
+# Drag and Drop into wherever you keep your Map Edits.
+
+Join the discord for more: https://discord.gg/YEY5E4hhRU
---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+author 'CertifiedStag'
+Description 'USA to UK Flags'
+version '1.0.0'
+this_is_a_map 'yes'
+
+files {
+    'stream/**/*.ytd',
+    'stream/**/*.ytf',
+}
+
+lua54 'yes'
